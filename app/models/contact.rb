ec029a2d6dfc43d810ff58410eae8431f5aef956
Albert Nikolli <albert_nikolli@icloud.com>
2024-07-06 22:22:39 +0200

    added the contacts model and edited the database for recieving messages

---
+++ app/models/contact.rb
@@ -0,0 +1,5 @@
+class Contact < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be a valid email address" }
+  validates :message, presence: true
+end
