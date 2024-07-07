81b0a6320fe6f6771e9fee3931f53f4812071c2d
Albert Nikolli <albert_nikolli@icloud.com>
2024-07-07 11:03:23 +0200

    resetting the database

--- app/models/contact.rb
+++
@@ -1,5 +0,0 @@
-class Contact < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be a valid email address" }
-  validates :message, presence: true
-end
