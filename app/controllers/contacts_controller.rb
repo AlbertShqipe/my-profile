e0a64e1ed2c4e72f2b79502274193416014e5656
Albert Nikolli <albert_nikolli@icloud.com>
2025-01-24 23:48:55 +0100

    finished all settings, all good

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,4 @@
 class ContactsController < ApplicationController
-  def new
-    @contact = Contact.new
-  end
-
-  def create
-    @contact = Contact.new(contact_params)
-    logger.debug "Contact params: #{contact_params.inspect}" # Add this line to debug
-    if @contact.save
-      redirect_to root_path, notice: "Your message has been sent successfully."
-    else
-      render :new
-    end
-  end
-
   def send_email
     # Ensure required params are present
     name = params[:name]
@@ -28,11 +14,5 @@ class ContactsController < ApplicationController
     end
 
     redirect_to root_path
-  end
-
-  private
-
-  def contact_params
-    params.permit(:name, :email, :message)
   end
 end
