c92087aade8e498eb3aef3100848b7ddfd8b50cb
Albert Nikolli <albert_nikolli@icloud.com>
2025-01-26 18:47:10 +0100

    Added all necessay changes to the webpage

--- app/javascript/controllers/burger_menu_controller.js
+++ app/javascript/controllers/burger_menu_controller.js
@@ -5,12 +5,21 @@ export default class extends Controller {
   connect() {
     const burgerMenu = document.getElementById('burger-menu');
     const menu = document.getElementById('menu-container');
+    const logo = document.getElementById('logoBoxArtistic');
+    const albert = document.getElementById('albert');
+    const logoContent = document.getElementById('navbarContent');
     burgerMenu.addEventListener('click', () => {
       burgerMenu.classList.toggle('active');
       if (burgerMenu.classList.contains('active')) {
         menu.style = 'opacity: 1; z-index:1000';
+        logo.style = 'display: none';
+        albert.style = 'display: none';
+        logoContent.style = 'display: none';
       } else {
         menu.style = 'display: none';
+        albert.style = 'display: block';
+        logo.style = 'display: block;';
+        // logoContent.style = 'display: block;';
       }
     });
   }
